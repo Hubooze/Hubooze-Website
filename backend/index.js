fcdd311ad56bb9455dbb6fd5156b1cac68852dcf
Hubooze <luckysolanki2013@gmail.com>
2024-07-04 14:49:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,18 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
 }));
 
+
 // Other Middlewares
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 //API creation
