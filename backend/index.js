c35e64691e043b2dfee5f23f06d4ec9ab9f7ecff
Hubooze <luckysolanki2013@gmail.com>
2024-09-14 15:12:18 +0530

    IP commits

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 // Configure CORS
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://3.111.119.80',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true,
